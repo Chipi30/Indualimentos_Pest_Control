67afd59c63b4e76a9613b4c5df6d4fa3d23388d3
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2023-07-06 10:24:25 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+
+// nuevos modulos
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    PasswordModule,
+    CheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+
+  user={
+    user: '',
+    password: ''
+  }
+
+
+  login(){
+    console.log(this.user.user);
+    console.log(this.user.password);
+
+
+  }
+
+
+  valCheck: string[] = ['remember'];
+
+  password!: string;
+
+  constructor(public layoutService: LayoutService) { }
+
+}
